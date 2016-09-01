3da351631dee5f0c29780166d5d3635e2a057bd3
vishrutjai <vishrutjaipuria@gmail.com>
2016-09-01 19:28:21 +0530

    Testing

--- inventorymanagement.py
+++ inventorymanagement.py
@@ -4,7 +4,7 @@ import os
 import time
 from datetime import datetime
 from emailengine import emailit
-#-------------------- CLASS DEFINATION --------------------------#
+#-------------------- CLASS DEFINATION ----------------------------------#
 
 class inventory(object):
 
